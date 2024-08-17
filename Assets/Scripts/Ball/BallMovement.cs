53bd4f5771bda6ace47988b516c65e3756cb0c4d
nearQ <go.ulloa@duocuc.cl>
2024-08-16 22:00:00 -0400

    Ball balance

--- Assets/Scripts/Ball/BallMovement.cs
+++ Assets/Scripts/Ball/BallMovement.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class BallPhysics : MonoBehaviour
 {
-    public float initialSpeed = 20f; 
-    public float frictionCoefficient = 0.90f; 
+    public float initialSpeed = 20f;
+    public float frictionCoefficient = 0.97f; 
+    public float maxSpeed = 400f;
     private Rigidbody2D rb;
     public AudioSource bounceSound;
     public AudioSource boostAudioSource;
@@ -13,29 +14,39 @@ public class BallPhysics : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 
-
         Vector2 initialDirection = new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f)).normalized;
         rb.velocity = initialDirection * initialSpeed;
     }
 
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.F))
         {
-            rb.velocity *= speedBoost; 
+            rb.velocity *= speedBoost;
             boostAudioSource.Play();
+        }
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+
         bounceSound.Play();
+
         Vector2 direction = rb.velocity.normalized;
 
         float angleFromHorizontal = Mathf.Abs(Vector2.Angle(direction, Vector2.right));
         float angleFromVertical = Mathf.Abs(Vector2.Angle(direction, Vector2.up));
 
         rb.velocity = direction.normalized * (rb.velocity.magnitude * frictionCoefficient);
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
     }
 }
