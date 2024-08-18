c69cd23372f539843b1507e37c1bdb5021ff200f
nearQ <go.ulloa@duocuc.cl>
2024-08-18 16:53:11 -0400

    cambios menores

---
+++ Assets/Scripts/Score/points.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private float score;
+
+    void getScore(float points)
+    {
+        score += points;
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
