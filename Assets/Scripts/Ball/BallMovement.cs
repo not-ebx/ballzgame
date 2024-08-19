0561e3c619d1aaaec98353e3ba705c14c565d349
nearQ <go.ulloa@duocuc.cl>
2024-08-19 02:01:56 -0400

    owo

--- Assets/Scripts/Ball/BallMovement.cs
+++ Assets/Scripts/Ball/BallMovement.cs
@@ -55,7 +55,7 @@ public class BallPhysics : MonoBehaviour
 
         newSpeed = currentSpeed + (speedBoost*damage);
 
-        var newHitlag = Mathf.Min(hitLag * newSpeed / 10, 3f);
+        var newHitlag = Mathf.Min(hitLag * newSpeed / 10, 2.2f);
         hitBox.Stop(newHitlag);
         
         rb.velocity = direction * Mathf.Min(newSpeed, maxSpeed);
